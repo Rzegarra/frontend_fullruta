27e5f4b9cb0df210cd053dbcc9b44eb55a102f24
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2016-10-06 19:33:57 -0500

    avanzando con el favicon

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express")
 var app = express()
 
+app.set('view engine', 'pug')
+
+app.use(express.static('public'));
+
 app.get('/', function (req,res) {
-  res.send('hola mundo')
+  res.render('index')
 })
+
 app.listen(3000, function (err) {
   if (err) return console.log('hubo un error'),process.exit(1);
   console.log('vego escuchando en el puerto 3000')
